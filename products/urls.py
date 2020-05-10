16d4fc1adedb6cb58612305907497ab0709751ec
Odong Geofrey <Ogeof49@gmail.com>
2020-05-10 11:24:17 +0300

    new change to urls

--- products/urls.py
+++ products/urls.py
@@ -3,21 +3,21 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #This would be a comment in Python
+    #Home url
     path('', views.index, name='index'),
     
-    #This would be a comment in Python
+    #Services urls
     path('web_design', views.web_design, name='web_design'), 
     path('graphics_design', views.graphics_design, name='graphics_design'), 
     path('code_auditing', views.code_auditing, name='code_auditing'),
     path('photography', views.photography, name='photography'), 
     
-    #This would be a comment in Python
+    #blog, about, contact urls
     path('blog', views.blog, name='blog'), 
     path('about', views.about, name='about'),
     path('contact', views.contact, name='contact'),  
     
         
-    #This would be a comment in Python
+    #blog details url
     path('<slug:slug>/', views.BlogDetail.as_view(), name='blog_detail'),
 ]
